bfbcabd56ec2dfc05362477f03d27e04f8c60750
isaac <aderonmuisaac@gmail.com>
2020-07-13 14:42:21 +0100

    chore: database config

--- backend/models.py
+++ backend/models.py
@@ -4,8 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 
 database_name = "trivia"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
-
+database_path = "postgres://{}:{}@{}/{}".format('cynepton', 'cynepton', 'localhost:5432', database_name)
+'postgresql://cynepton:cynepton@localhost:5432/fyyur'
 db = SQLAlchemy()
 
 '''
