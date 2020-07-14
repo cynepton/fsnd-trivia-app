39747aade812651e958e154e61c82fcf5217d9bf
isaac <aderonmuisaac@gmail.com>
2020-07-14 14:46:10 +0100

    feat: test /categories endpoint

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,8 +15,15 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://{}:{}@{}/{}".format('cynepton', 'cynepton',  'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
+
+        self.new_question = {
+            'question': 'What is Batman Beyonds secret Identity',
+            'answer': 'Terry Mcginnis',
+            'difficulty': 3,
+            'category': 5
+        }
 
         # binds the app to the current context
         with self.app.app_context():
@@ -33,7 +40,23 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_all_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
 
+        print(data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertGreaterEqual(len(data['categories']), 0)
+    '''
+    def test_404_sent_requesting_beyond_valid_page(self):
+        res = self.client().get('/books?page=1000', json={'rating': 1})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+    '''
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
