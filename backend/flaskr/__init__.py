43e97eccbc433757d5481c5f0d1ba6886f63d008
isaac <aderonmuisaac@gmail.com>
2020-07-15 14:44:19 +0100

    style: PEP8 style compliance

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -29,7 +29,7 @@ def create_app(test_config=None):
     @TODO: Set up CORS. Allow '*' for origins.
     Delete the sample route after completing the TODOs
     '''
-    #cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+    # cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
     '''
     @TODO: Use the after_request decorator to set Access-Control-Allow
@@ -37,9 +37,9 @@ def create_app(test_config=None):
     @app.after_request
     def after_request(response):
         response.headers.add('Access-Control-Allow-Headers',
-            'Content-Type,Authorization,true')
+                             'Content-Type,Authorization,true')
         response.headers.add('Access-Control-Allow-Methods',
-            'GET,PUT,POST,DELETE,OPTIONS')
+                             'GET,PUT,POST,DELETE,OPTIONS')
         return response
 
     '''
@@ -63,7 +63,7 @@ def create_app(test_config=None):
     Create an endpoint to handle GET requests for questions,
     including pagination (every 10 questions).
     This endpoint should return a list of questions,
-    number of total questions, current category, categories. 
+    number of total questions, current category, categories.
 
     TEST: At this point, when you start the application
     you should see questions and categories generated,
@@ -95,12 +95,12 @@ def create_app(test_config=None):
         })
 
     '''
-    @TODO: 
-    Create an endpoint to DELETE question using a question ID. 
+    @TODO:
+    Create an endpoint to DELETE question using a question ID.
 
-    TEST: When you click the trash icon next to a question, 
+    TEST: When you click the trash icon next to a question,
     the question will be removed.
-    This removal will persist in the database and when you refresh the page. 
+    This removal will persist in the database and when you refresh the page.
     '''
     @app.route('/questions/<int:question_id>', methods=['DELETE'])
     def delete_question(question_id):
@@ -126,14 +126,15 @@ def create_app(test_config=None):
             abort(400)
 
     '''
-    @TODO: 
-    Create an endpoint to POST a new question, 
-    which will require the question and answer text, 
+    @TODO:
+    Create an endpoint to POST a new question,
+    which will require the question and answer text,
     category, and difficulty score.
 
-    TEST: When you submit a question on the "Add" tab, 
-    the form will clear and the question will appear at the end of the last page
-    of the questions list in the "List" tab.  
+    TEST: When you submit a question on the "Add" tab,
+    the form will clear and
+    the question will appear at the end of the last page
+    of the questions list in the "List" tab.
     '''
     @app.route('/questions', methods=['POST'])
     def post_question():
@@ -160,14 +161,14 @@ def create_app(test_config=None):
         except Exception:
             abort(400)
     '''
-    @TODO: 
-    Create a POST endpoint to get questions based on a search term. 
-    It should return any questions for whom the search term 
-    is a substring of the question. 
+    @TODO:
+    Create a POST endpoint to get questions based on a search term.
+    It should return any questions for whom the search term
+    is a substring of the question.
 
-    TEST: Search by any phrase. The questions list will update to include 
-    only question that include that string within their question. 
-    Try using the word "title" to start. 
+    TEST: Search by any phrase. The questions list will update to include
+    only question that include that string within their question.
+    Try using the word "title" to start.
     '''
     @app.route('/searchquestions', methods=['POST'])
     def search_quetions():
@@ -192,12 +193,12 @@ def create_app(test_config=None):
             })
 
     '''
-    @TODO: 
-    Create a GET endpoint to get questions based on category. 
+    @TODO:
+    Create a GET endpoint to get questions based on category.
 
-    TEST: In the "List" tab / main screen, clicking on one of the 
-    categories in the left column will cause only questions of that 
-    category to be shown. 
+    TEST: In the "List" tab / main screen, clicking on one of the
+    categories in the left column will cause only questions of that
+    category to be shown.
     '''
     @app.route('/categories/<int:category_id>/questions')
     def get_by_category(category_id):
@@ -217,17 +218,16 @@ def create_app(test_config=None):
         except Exception:
             abort(400)
 
-
     '''
-    @TODO: 
-    Create a POST endpoint to get questions to play the quiz. 
-    This endpoint should take category and previous question parameters 
-    and return a random questions within the given category, 
-    if provided, and that is not one of the previous questions. 
+    @TODO:
+    Create a POST endpoint to get questions to play the quiz.
+    This endpoint should take category and previous question parameters
+    and return a random questions within the given category,
+    if provided, and that is not one of the previous questions.
 
     TEST: In the "Play" tab, after a user selects "All" or a category,
     one question at a time is displayed, the user is allowed to answer
-    and shown whether they were correct or not. 
+    and shown whether they were correct or not.
     '''
     @app.route('/quizzes', methods=['POST'])
     def post_quizzes():
@@ -274,7 +274,7 @@ def create_app(test_config=None):
                     })
                 else:
                     next_question = all_questions[0]
-                    
+
         print('-----------------------------------------------')
         print(next_question.format())
         print('-----------------------------------------------')
@@ -284,41 +284,40 @@ def create_app(test_config=None):
         })
 
     '''
-    @TODO: 
-    Create error handlers for all expected errors 
-    including 404 and 422. 
+    @TODO:
+    Create error handlers for all expected errors
+    including 404 and 422.
     '''
     @app.errorhandler(400)
     def not_found(error):
         return jsonify({
-        'success': False,
-        'error': 400,
-        'message': 'Bad Request'
+            'success': False,
+            'error': 400,
+            'message': 'Bad Request'
         }), 400
 
     @app.errorhandler(404)
     def not_found(error):
         return jsonify({
-        'success': False,
-        'error': 404,
-        'message': 'Resource not found'
+            'success': False,
+            'error': 404,
+            'message': 'Resource not found'
         }), 404
 
     @app.errorhandler(406)
     def not_found(error):
         return jsonify({
-        'success': False,
-        'error': 406,
-        'message': 'Not Acceptable'
+            'success': False,
+            'error': 406,
+            'message': 'Not Acceptable'
         }), 406
 
     @app.errorhandler(422)
     def unprocessable(error):
         return jsonify({
-        'success': False,
-        'error': 422,
-        'message': 'unprocessable'
+            'success': False,
+            'error': 422,
+            'message': 'unprocessable'
         }), 422
-    
 
     return app
